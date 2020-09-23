d425863b2146f5348d1c66d7c74904ad7a121cc1
anikett-rpx <anikett_c@rpxcorp.com>
2020-09-24 00:33:14 +0530

    slot maching

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import SlotM from './SlotMach';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return(
+    <>
+    <h1 className="heading_style"> 🎰 Welcome to <span style={{fontWeight: 'bold'}}> Slot Machine game</span> 🎰 </h1>  
+      <div className="slotmachine">   
+        <SlotM x='😄' y='😄' z='😄' />
+        <hr/>
+        <SlotM x='😸' y ='🐕' z='🥘'/>
+        <hr/>
+        <SlotM x='🐴' y='🌵' z='🍎'/>
+      </div>
+    </>
+  )
 }
 
 export default App;
